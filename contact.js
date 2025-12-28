c0756a8ee9720a9c171c52c8054383ede8d8b306
Elin Nilsson <elin@formatfabriken.se>
2025-12-28 10:32:33 +0100

    Add: References to all form elements and stored them in variables.

---
+++ contact.js
@@ -0,0 +1,5 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const subjectsList = document.getElementById('subjectsList');
+const message = document.getElementById('message');
