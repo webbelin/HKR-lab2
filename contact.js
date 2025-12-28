a1cb67ed5b46521351ed1c47b2b584bbc2c2b805
Elin Nilsson <elin@formatfabriken.se>
2025-12-29 00:58:00 +0100

    Add: Added regex for checking only letters and a function for validating firstName field and testing it

--- contact.js
+++ contact.js
@@ -3,3 +3,14 @@ const lastName = document.getElementById('lastName');
 const email = document.getElementById('email');
 const subjectsList = document.getElementById('subjectsList');
 const message = document.getElementById('message');
+
+const onlyLettersRegex = /^[A-Za-z]+$/;
+
+/* Validate fields */
+function validateName(){
+    if(onlyLettersRegex.test(firstName.value)){
+        console.log('Bra');
+    } else {
+        console.log('Dåligt')
+    }
+}
