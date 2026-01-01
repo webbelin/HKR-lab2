3d3f1dddc4100321e0c0b772aff9f0815f2f740e
Elin Nilsson <elin@formatfabriken.se>
2026-01-02 00:39:52 +0100

    Add: Message character counter.

--- contact.js
+++ contact.js
@@ -4,6 +4,16 @@ const email = document.getElementById('email');
 const subjectsList = document.getElementById('subjectsList');
 const message = document.getElementById('message');
 
+/* Message counter */
+message.addEventListener('keyup', function(){
+    let counter = this.value.length;
+    document.getElementById('counterText').textContent = counter;
+
+    if(counter == 20){
+        validateMessage();
+    }
+});
+
 const onlyLettersRegex = /^[A-Za-z]+$/;
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
@@ -12,7 +22,8 @@ function validateName(){
     if(onlyLettersRegex.test(firstName.value)){
         console.log('Bra');
     } else {
-        console.log('Dåligt')
+        const text = 'Kontrollera att namnet är rättstavat.';
+        showError(text);
     }
 }   
 
@@ -20,7 +31,8 @@ function validateEmail(){
     if(emailRegex.test(email.value)){
         console.log('Bra');
     } else {
-        console.log('Dåligt')
+        const text = 'Kontrollera email..';
+        showError(text);
     }
 }   
 
@@ -28,18 +40,14 @@ function validateMessage(){
     if(message.value.length > 3){
         console.log('Godkänt medd')
     } else {
-        console.log('ej godkänt')
-        const text = 'textmeddelande';
-        showError(text)
+        const text = 'Meddelandet måste innehålla minst 20 tecken.';
+        showError(text);
     }
 }
 
 function showError(text){
-    console.log('Här är: ', text);
-}
-
-function clearError(){
-
+    const errorMessage = document.getElementsByClassName('errorMessage');
+    errorMessage.innerText = text;
 }
 
 function clearForm(){
