6f9ea5ad4d86ae9aaf38ff16507dd72a84b82bee
Elin Nilsson <elin@formatfabriken.se>
2026-01-05 21:45:38 +0100

    Refactor: formatting and some cleanup

--- contact.js
+++ contact.js
@@ -5,21 +5,28 @@ const email = document.getElementById('email');
 const subjectsList = document.getElementById('subjectsList');
 const message = document.getElementById('message');
 
+/* Regex */
+const onlyLettersRegex = /^[A-Za-z]+$/;
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
 /* Message character counter */
 message.addEventListener('keyup', function(){
-        let counter = this.value.length;
-        document.getElementById('counterText').textContent = counter + ' / 20 characters';
-        document.getElementById('counterText').classList.remove('color-change');
+    let counter = this.value.length;
+    document.getElementById('counterText').textContent = counter + ' / 20 characters';
+    document.getElementById('counterText').classList.remove('color-change');
+
     if(counter >= 20){
         document.getElementById('counterText').classList.add('color-change');
     }
 });
 
-/* Submit */
+/* Submit form */
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
     if(validateName() && validateEmail() && validateMessage()){
+
+        document.getElementById('noSuccessMessage').innerText = '';
 
         // Success message
         document.getElementById('successMessage').innerText = `Thank you ${firstName.value}! I will contact you soon!`;
@@ -34,22 +41,19 @@ form.addEventListener('submit', function(event){
         console.log('false');
         document.getElementById('noSuccessMessage').innerText = 'Check all fields';
     }
-
 });
 
-const onlyLettersRegex = /^[A-Za-z]+$/;
-const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-/* Validate fields */
+/* ******************************* FUNCTIONS ******************************* */
+
 function validateName(){
     if(onlyLettersRegex.test(firstName.value)){
         firstName.classList.add('valid');
         firstName.classList.remove('error');
-                const text = '';
-                showError(text);
-                console.log('Valid name');
-                return true;
-
+        const text = '';
+        showError(text);
+        console.log('Valid name');
+        return true;
     } else {
         firstName.classList.add('error');
         firstName.classList.remove('valid');
@@ -57,40 +61,37 @@ function validateName(){
         showError(text);
         console.log('Invalid name');
         return false;
-    }
-         
+    }    
 }   
 
 function validateEmail(){
  if(emailRegex.test(email.value)){
         email.classList.add('valid');
         email.classList.remove('error');
-         return true;
+        return true;
     } else {
         email.classList.add('error');
         email.classList.remove('valid');
         const text = 'Check that the email is correct.';
         showError(text);
-         return false;
+        return false;
     }
-
 }   
 
 function validateMessage(){
     if(message.value.length >= 20){
         message.classList.add('valid');
         message.classList.remove('error');
-         console.log('Valid message');
-         return true;
+        console.log('Valid message');
+        return true;
     } else {
         message.classList.add('error');
         message.classList.remove('valid');
         const text = 'The message must contain at least 20 characters.';
         showError(text);
-         console.log('Invalid message');
-         return false;
+        console.log('Invalid message');
+        return false;
     }
-
 }
 
 function showError(text){
